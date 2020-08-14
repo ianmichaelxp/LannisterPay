088886b56474fe91ed64096427eabcc5ebad4cf0
Ian Michael <ianmichael.xp@gmail.com>
2020-08-13 22:07:46 -0300

    Organizando o código em módulos

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,4 +1,9 @@
-class NegociacaoController
+import { Negociacoes } from '../models/Negociacoes'
+import { NegociacoesView } from '../views/NegociacoesView'
+import { MensagemView } from '../views/MensagemView'
+import { Negociacao } from '../models/Negociacao'
+
+export class NegociacaoController
 {
     private _inputData: JQuery;
     private _inputQuantidade: JQuery;
