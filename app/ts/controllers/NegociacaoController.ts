838a81cb63244fc9f640e2d4cfa6518acac6a2e9
Ian Michael <ianmichael.xp@gmail.com>
2020-08-19 12:35:51 -0300

    Fetch API

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,6 +1,7 @@
 import { Negociacoes, Negociacao} from '../models/index';
 import { NegociacoesView, MensagemView } from '../views/index';
 import { domInject } from '../helpers/decorators/domInject';
+import { now } from 'jquery';
 
 
 export class NegociacaoController
@@ -40,6 +41,28 @@ export class NegociacaoController
 
     importarDados()
     {
-        alert('oi');
+        function isOk(res: Response)
+        {
+            if(res.ok)
+            {
+                return res;
+            }
+            else
+            {
+                throw new Error(res.statusText)
+            }
+        }
+
+        fetch('http://localhost:8080/dados')
+            .then(res => isOk(res))
+            .then(res => res.json())
+            .then((dados: any[]) =>  
+                {
+                    dados
+                        .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
+                        .forEach(negociacao => this._negociacoes.adiciona(negociacao));
+                    this._negociacoesView.update(this._negociacoes);
+                })
+            .catch(err => console.log(err.message));
     }
 }
